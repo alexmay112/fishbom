58304ad0e2ae2605bd067b1c865b3dec996e8071
O.M <a.majstrenko@everad.com>
2019-12-19 14:13:53 +0200

    add warranty section

---
+++ app/components/block-warranty/block-warranty.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('.photo-slider').slick({
+    // autoplay: true,
+    autoplaySpeed: 1000,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    speed: 2000,
+    // centerMode: true,
+    // pauseOnFocus: false,
+    // focusOnSelect: true
+  });
+});
