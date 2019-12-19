3ef2d7725fd657566411d6ad2790897d8c0510a8
O.M <a.majstrenko@everad.com>
2019-12-19 16:01:29 +0200

    add adaptive to warranty section

--- app/components/block-warranty/block-warranty.js
+++ app/components/block-warranty/block-warranty.js
@@ -1,12 +1,42 @@
 $(document).ready(function () {
   $('.photo-slider').slick({
-    // autoplay: true,
-    autoplaySpeed: 1000,
+    autoplay: true,
     slidesToShow: 7,
     slidesToScroll: 1,
-    speed: 2000,
-    // centerMode: true,
+    speed: 1000,
+    centerMode: true,
+    autoplaySpeed: 0,
+    speed: 10000,
+    cssEase: 'linear',
     // pauseOnFocus: false,
-    // focusOnSelect: true
+    draggable: false,
+    focusOnSelect: true,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 481,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 321,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
   });
 });
