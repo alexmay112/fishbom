a04684763afe5e147a44e3b5280bf3be49e1a1a8
O.M <a.majstrenko@everad.com>
2019-12-23 18:55:24 +0200

    mo

--- app/components/block-report/block-report.js
+++ app/components/block-report/block-report.js
@@ -1,18 +1,3 @@
-// $(document).ready(function () {
-//   $('.r-slider').slick({
-//     infinite: true,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     adaptiveHeight: true,
-//     dots: true,
-//     centerMode: true,
-//     centerPadding: 0,
-//     infinite: true,
-//     nextArrow: '.arrows__button_right',
-//     prevArrow: '.arrows__button_left',
-//   });
-// });
-
 var sliderSelector = '.swiper-container',
   options = {
     loop: true,
@@ -23,8 +8,8 @@ var sliderSelector = '.swiper-container',
     effect: 'coverflow', // 'cube', 'fade', 'coverflow',
     coverflowEffect: {
       rotate: 0, // Slide rotate in degrees
-      stretch: 551, // Stretch space between slides (in px)
-      depth: 860, // Depth offset in px (slides translate in Z axis)
+      stretch: 719, // Stretch space between slides (in px) :551
+      depth: 425, // Depth offset in px (slides translate in Z axis) :860
       modifier: 1, // Effect multipler
       slideShadows: false,
     },
@@ -39,21 +24,22 @@ var sliderSelector = '.swiper-container',
       prevEl: '.swiper-button-prev',
     },
     breakpoints: {
-      1189: {
+      1239: {
         coverflowEffect: {
-          depth: 700, // Depth offset in px (slides translate in Z axis)
-        }
+          stretch: 666, // Stretch space between slides (in px) :551
+          depth: 425, // Depth offset in px (slides translate in Z axis) :860
+          modifier: 1, // Effect multipler
+          slideShadows: false,
+        },
       },
       1023: {
-        coverflowEffect: {
-          depth: 800, // Depth offset in px (slides translate in Z axis)
-        }
+        effect: 'slide'
       },
-      767: {
-        coverflowEffect: {
-          depth: 0, // Depth offset in px (slides translate in Z axis)
-        }
-      }
+      // 767: {
+      //   coverflowEffect: {
+      //     depth: 0, // Depth offset in px (slides translate in Z axis)
+      //   }
+      // }
     },
     // Events
     on: {
