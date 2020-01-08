7b582b36eb34448de9907a4041f8119576eaef6c
O.M <a.majstrenko@everad.com>
2020-01-08 11:46:08 +0200

    fix NY

--- app/components/block-report/block-report.js
+++ app/components/block-report/block-report.js
@@ -26,7 +26,7 @@ var sliderSelector = '.swiper-container',
     breakpoints: {
       1239: {
         coverflowEffect: {
-          stretch: 686, // Stretch space between slides (in px)
+          stretch: 666, // Stretch space between slides (in px)
           depth: 425, // Depth offset in px (slides translate in Z axis)
           modifier: 1, // Effect multipler
           slideShadows: false,
@@ -43,4 +43,47 @@ var sliderSelector = '.swiper-container',
       }
     }
   };
+
+//Создаем объект 'user', который будет содержать информацию Detect.js
+//Вызываем detect.parse() с navigator.userAgent в качестве аргумента
+var user = detect.parse(navigator.userAgent);
+
+// Выводим нужные значения в консоли браузера
+console.log(user.browser.family)
+
+if (user.browser.family == 'IE') {
+  var sliderSelector = '.swiper-container',
+    options = {
+      loop: true,
+      speed: 700,
+      slidesPerView: 'auto', // or 'auto'
+      autoHeight: true,
+      centeredSlides: true,
+      effect: 'fade', // 'cube', 'fade', 'coverflow',
+      coverflowEffect: {
+        rotate: 0, // Slide rotate in degrees
+        stretch: 719, // Stretch space between slides (in px)
+        depth: 425, // Depth offset in px (slides translate in Z axis) *425
+        modifier: 1, // Effect multipler
+        slideShadows: false,
+      },
+      grabCursor: true,
+      // parallax: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      // Events
+      on: {
+        imagesReady: function () {
+          this.el.classList.remove('loading');
+        }
+      }
+    };
+}
+
 var mySwiper = new Swiper(sliderSelector, options);
